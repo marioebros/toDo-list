596bb262c3ffd313108e1a6b1f1d8b705a031d9e
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-05 18:07:57 -0700

    imported load-blank thru index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import _ from "lodash";
+import { createList } from "./create-list.js";
+import { loadBlank } from "./load-blank.js";
+import { domManip } from "./dom-manip";
 
 // Testing out webpack initial setup of index.js
 console.log("testing index.js webpack");
 
-// Testing out DOM manipulation via webpack
-const contentDiv = document.querySelector(".content");
-const testH1 = document.createElement("h1");
-testH1.textContent = "HELLO MUNDO...Test from Webpack.js DOM Manipulation";
-contentDiv.appendChild(testH1);
+// Call loadBlank upon first landing
+loadBlank();
+
+// Call DOM Manipulation module to control UI
+domManip();
+
+// TODO: Click event modeul HERE for todo and project creations
+
+// Call create-list.js module file and apply some objects/properties  (this will eventually)
+const myToDo = createList("Grocery Run", "Go get groceries", "10/15/2022");
+const myToDo2 = createList("Work");
+console.log("Show me properties on myToDo from index.js.....", myToDo);
+console.log("Show me properties on myToDo2 from index.js.....", myToDo2);
 
 // function component() {
 //   const element = document.createElement("div");
