d6adcf4c5435bce8a73060c7a61b8a138c7bc281
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-10-06 16:07:22 -0700

    updated create-list, index.html placeholder

--- src/create-list.js
+++ src/create-list.js
@@ -1,19 +1,56 @@
+import { compareAsc, format, parseISO, startOfToday } from "date-fns";
+import { clearForm } from "./dom-manip";
+
 // Creating an array for ToDo, if needed later on
 let toDoArray = [];
 
 // Factory function to create ToDo list
-export const createList = (
-  Title,
-  Description,
-  DueDate,
-  Priority,
-  CheckList
-) => {
-  console.log("Called createList module... creating ToDo now");
+export const createList = () => {
+  let Title = document.getElementById("Title").value;
+  let Description = document.getElementById("Description").value;
+  let DueDate = document.getElementById("DueDate").value;
+  let Priority = document.getElementById("Priority").value;
+
+  // Check to see if empty fields exist
+  if (Title == "" || Description == "" || DueDate == "") {
+    alert("Title, Description, ad Due Date are required fields, try again!");
+    return;
+  }
+
+  // Check to see if a pre-date was entered
+  if (parseISO(DueDate) < startOfToday()) {
+    alert(
+      "You have entered a date that has already passed! Please enter a valid date"
+    );
+    console.log("due date", parseISO(DueDate));
+    console.log("date now", startOfToday());
+    return;
+  }
+
+  // Loop over the nodelist for Checklist items from the DOM and format to string
+  const nodeListCheckList = document.querySelectorAll("li");
+  let CheckListArray = [];
+  for (let i = 0; i < nodeListCheckList.length; i++) {
+    // Strip Off the "x" from end of each li then push to temp array
+    let strippedCheckList = nodeListCheckList[i].textContent.replace(
+      "\u00D7",
+      ""
+    );
+    CheckListArray.push(strippedCheckList);
+  }
+
+  // Strip Off the checklist array and convert to string with commas
+  let CheckList = CheckListArray.join(", ");
+
+  console.log("Called createList module... Creating ToDo now");
   console.log({ Title, Description, DueDate, Priority, CheckList });
   console.log("Pushing this object to the toDoArray...");
-  // TODO: Remove below two lines of code if array is not needed
+  // TODO: Remove the following two lines of code if array is NOT needed
   toDoArray.push({ Title, Description, DueDate, Priority, CheckList });
   console.log(toDoArray);
+
+  // RESET the form after successful submission
+  clearForm();
+
   return { Title, Description, DueDate, Priority, CheckList };
 };
--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ loadBlank();
 // domManip();
 
 // Click Events Module
-let clickEventModule = function () {
+let clickEventModule = (function () {
   // Click Event for displaying the form
   const addNewToDo = document.querySelector(".add-todo-button");
   addNewToDo.addEventListener("click", displayTheForm);
@@ -28,25 +28,29 @@ let clickEventModule = function () {
   // Click Event to clear the form
   const clearButton = document.querySelector(".reset-button");
   clearButton.addEventListener("click", clearForm);
-};
+
+  // Click Event to submit a new todo form to project
+  const submitButton = document.querySelector(".submit-button");
+  submitButton.addEventListener("click", createToDo);
+})();
 
 // Call create-list.js module file and apply some objects/properties  (this will eventually be replaced by UI input)
-const myToDo = createList(
-  "Grocery Run",
-  "Go get groceries",
-  "10/15/2022",
-  "Low",
-  "Meats, Eggs, Millk"
-);
-const myToDo2 = createList(
-  "Work",
-  "Do Odin Project work",
-  "10/20/2022",
-  "Medium",
-  "Read Module, Finish ToDo Project"
-);
-console.log("Show me properties on myToDo from index.js.....", myToDo);
-console.log("Show me properties on myToDo2 from index.js.....", myToDo2);
+// const myToDo = createList(
+//   "Grocery Run",
+//   "Go get groceries",
+//   "10/15/2022",
+//   "Low",
+//   "Meats, Eggs, Millk"
+// );
+// const myToDo2 = createList(
+//   "Work",
+//   "Do Odin Project work",
+//   "10/20/2022",
+//   "Medium",
+//   "Read Module, Finish ToDo Project"
+// );
+// console.log("Show me properties on myToDo from index.js.....", myToDo);
+// console.log("Show me properties on myToDo2 from index.js.....", myToDo2);
 
 // Lodash, now imported by this script
 element.innerHTML = _.join(["Hello", "webpack"], " ");
